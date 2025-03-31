f8e12d3491600ff050e92cbb68efbf6befeac9ac
Ch1charito <r.buschmann.work@gmail.com>
2025-03-31 17:41:20 +0200

    Overlay hinzugefügt

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-let imgPlaces = [
-    "alaska.jpg",
-    "borabora.jpg",
-    "camp.jpg",
-    "munich.jpg",
-    "nyc.jpg",
-    "rio.jpg",
-    "taj.jpg",
-    "uae.jpg",
-]
-
 let img = [
-    "./img/bilder/image1.jpg",
-    "./img/bilder/image2.jpg",
-    "./img/bilder/image3.jpg",
-    "./img/bilder/image4.jpg",
-]
-
-
-
-
+  "./img/bilder/image1.jpg",
+  "./img/bilder/image2.jpg",
+  "./img/bilder/image3.jpg",
+  "./img/bilder/image4.jpg",
+  "./img/bilder/image5.jpg",
+  "./img/bilder/image6.jpg",
+  "./img/bilder/image7.jpg",
+  "./img/bilder/image8.jpg",
+  "./img/bilder/image9.jpg",
+  "./img/bilder/image10.jpg",
+  "./img/bilder/image11.jpg",
+  "./img/bilder/image12.jpg",
+];
 
 function render() {
-    let contentRef = document.getElementById('content');
-    for (let i = 0; i < img.length; i++){
-        contentRef.innerHTML += `
-        <div>
-            <img src="${img[i]} alt="Bild ${i + 1}">
-        </div>`
-    }
-};
+  let contentRef = document.getElementById("content");
+  for (let i = 0; i < img.length; i++) {
+    contentRef.innerHTML += `
+        <div onclick="toggleOverlay()">
+            <img src="${img[i]}" alt="Bild ${i + 1}">
+        </div>`;
+  }
+}
 
+function toggleOverlay() {
+  let overlayRef = document.getElementById("imgoverlay");
 
+  overlayRef.classList.toggle("d_none");
+}
