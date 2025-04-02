b9c103f37a46955f5cecb716c1eac25bf3a69cf1
Ch1charito <r.buschmann.work@gmail.com>
2025-04-02 15:17:32 +0200

    Function für Direction-btn hinzugefügt

--- script.js
+++ script.js
@@ -15,44 +15,59 @@ let img = [
 
 
 
+
+
 function render() {
-  let contentRef = document.getElementById("content");
+  let contentRef = document.getElementById("content");  // um die Elemente im Array auf der Seite durch eine Schleife zu rendern
   for (let i = 0; i < img.length; i++) {
     contentRef.innerHTML += singlePictures(i);
   }
 }
 
-function singlePictures(i) {
+function singlePictures(i) {          // um den Inhalt den man in die Schleife setzt zu bestimmen
     return `<div onclick="toggleOverlay(${i})">
                 <img src="${img[i]}" alt="Bild ${i + 1}">
             </div>`;
     
 }
 
-function toggleOverlay(index) {
+function toggleOverlay(index) {     // um das Overlay zu togglen
   let overlayRef = document.getElementById("imgoverlay");
-  overlayRef.innerHTML = "";
-  overlayRef.innerHTML += `<button class="close-btn" onclick="toggleOverlay(${index})">X</button>
-                            <div class="cardview">
-                              <button onclick="moveLeft()" class="switch-btn"><</button>
-                              <img id="overlaypic" src="${img[index]}">
-                              <button onclick="moveRight()" class="switch-btn">></button>
-                            </div>`;
+  renderOverlay(index);
   overlayRef.classList.toggle("d_none");
   
 }
 
-function moveRight(index) {
-  let moveRightRef = document.getElementById("overlaypic");
-  moveRightRef.innerHTML += `<button class="close-btn" onclick="toggleOverlay(${index + 1})">X</button>
-                              <div class="cardview">
-                                <button onclick="moveLeft()" class="switch-btn"><</button>
-                                <img id=overlaypic src="${img[index]}">
-                                <button onclick="moveRight()" class="switch-btn">></button>
-                              </div>`
-
+function renderOverlay(index) {  // um den Inhalt des Overlays zu bestimmen
+  let overlayRef = document.getElementById("imgoverlay");
+  overlayRef.innerHTML = "";  // den Container leeren bevor die function ausgeführt wird
+  overlayRef.innerHTML += `<button class="close-btn" onclick="toggleOverlay(${index})">X</button>
+                            <div class="cardview">
+                              <button onclick="moveLeft(${index})" class="switch-btn"><</button>
+                              <img id ="bigpic" src="${img[index]}">
+                              <button onclick="moveRight(${index})" class="switch-btn">></button>
+                            </div>`;
+  
 }
 
-function moveLeft() {
+function moveRight(i) {
+  if (i >= img.length - 1) { // Wenn i gleich oder größer als das letzte Element ist
+    i = 0; // setzt i auf das erste Element zurück
+  } else {
+    i = i + 1; // geht zum nächsten Element
+  }
+  renderOverlay(i);
+    
+  
+}
+
+function moveLeft(i) {
+  if (i === 0) { // Wenn i gleich 0 ist
+    renderOverlay(img.length - 1); // setzt i auf das letzte Element zurück
+    
+  } else {
+    renderOverlay(i-1); // geht zum voherigen Element
+  }
+  
 
 }
